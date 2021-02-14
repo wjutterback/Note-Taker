969c2e1c16664b3c50db121589721c965af49edb
Will Utterback <uwill08@gmail.com>
2021-02-14 14:47:06 -0600

    API call now responds with data from db.json

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3030;
 const server = require('http').Server(app);
+const fs = require('fs');
+const notes = JSON.parse(fs.readFileSync('./db/db.json'));
 
 app.use(express.static('public'));
 
@@ -11,6 +13,15 @@ app.get('/', (req, res) => {
 });
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  // should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+  // Each note needs uuid value.
 });
 
 server.listen(PORT, () => {
