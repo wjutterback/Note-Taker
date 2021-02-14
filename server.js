d2cd7274e20183836232a4eabb8c6aca918f2ae8
Will Utterback <uwill08@gmail.com>
2021-02-14 17:06:18 -0600

    Installed bodyParser and initialized it, successfully added new note to array in db.json

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const app = express();
 const PORT = process.env.PORT || 3030;
 const server = require('http').Server(app);
 const fs = require('fs');
-const notes = JSON.parse(fs.readFileSync('./db/db.json'));
-
+const bodyParser = require('body-parser');
 app.use(express.static('public'));
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -16,10 +17,20 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('./db/db.json'));
   res.json(notes);
 });
 
+//TODO: add unique ID, return new note to the client
 app.post('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+  notes.push(req.body);
+  const stringNote = JSON.stringify(notes);
+  console.log(stringNote);
+  console.log('note', notes);
+
+  fs.writeFileSync('./db/db.json', stringNote);
+
   // should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
   // Each note needs uuid value.
 });
